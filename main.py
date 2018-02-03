682cfa2022864c0a601f6379ddcb973328845e66
AlexOdlin <rockguitarist94alex@gmail.com>
2018-02-03 17:34:20 +0300

    create crazy buttom

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from tkinter import *
+from random import *
 
-print('Test!')
+def button_clicked():
+    print(button['bg'])
+    bg = '#%0x%0x%0x' % (int(random()*16), int(random()*16), int(random()*16))
+    button['bg'] = bg
+    button['activebackground'] = bg
+
+root = Tk()
+button = Button(root, command=button_clicked)
+button.pack()
+root.mainloop()
